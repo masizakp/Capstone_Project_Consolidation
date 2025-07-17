5f7d6372c7f57c33430006de7fa3ce3b3d4df897
masizakp <145618612+masizakp@users.noreply.github.com>
2025-07-17 11:01:40 +0200

    Initial commit - set up Django project with .gitignore

--- user_auth/apps.py
+++ user_auth/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class PersonalConfig(AppConfig):
+class UserAuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "personal"
-   
+    name = "user_auth"
