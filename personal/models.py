0cf0789310c056d163d22ca833ed3bdf8a1662a7
masizakp <145618612+masizakp@users.noreply.github.com>
2025-07-17 11:46:18 +0200

    Add indented docstring to Contact model

--- personal/models.py
+++ personal/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 class Contact(models.Model):
+    """
+    Model representing a contact/order entry.
+
+    :param name: First name of the contact
+    :type name: str
+    :param lastname: Last name of the contact
+    :type lastname: str
+    :param email: Email address of the contact
+    :type email: str
+    :param address: Physical address of the contact
+    :type address: str
+    :param product: Product ordered
+    :type product: str
+    :param payment_slip: Optional file upload of the payment slip
+    :type payment_slip: File, optional
+
+    :return: String representation combining name, lastname, and product
+    :rtype: str
+    """
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.EmailField()
