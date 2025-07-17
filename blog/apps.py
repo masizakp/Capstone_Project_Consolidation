3f940434271b5bd81192194908b64c9e4e177d6f
masizakp <145618612+masizakp@users.noreply.github.com>
2025-07-17 11:37:10 +0200

    Add indented docstring to BlogConfig class

--- blog/apps.py
+++ blog/apps.py
@@ -1,3 +1,9 @@
+"""
+    Configuration for the blog application.
+
+    Sets the default primary key field type for models to BigAutoField.
+    Specifies the app name as 'blog'.
+    """
 from django.apps import AppConfig
 
 
